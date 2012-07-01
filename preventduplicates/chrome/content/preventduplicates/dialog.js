ce32e3c500b66b953a4e5e2b7721340e086b599e
Chris Johnson-Roberson <chris.johnson.roberson@gmail.com>
2012-06-30 20:29:32 -0400

    switch to HTML citations

--- preventduplicates/chrome/content/preventduplicates/dialog.js
+++ preventduplicates/chrome/content/preventduplicates/dialog.js
@@ -2,14 +2,13 @@ Zotero_PreventDuplicates_Dialog = function () {};
 
 Zotero_PreventDuplicates_Dialog.init = function () {
 
-	var container = document.getElementById('zotero-prevent-duplicates-container');
+	var container = document.getElementById("zotero-prevent-duplicates-container");
 	
 	this.io = window.arguments[0];
-	var description = document.getElementById('zotero-prevent-duplicates-description');
-	
-	for(var i in this.io.dataIn) {
-		description.textContent += this.io.dataIn[i];
-	}
+	var description = document.getElementById("zotero-prevent-duplicates-description");
+	var desc_div = document.createElementNS("http://www.w3.org/1999/xhtml","div");
+	desc_div.innerHTML = this.io.dataIn["html"];
+	description.appendChild(desc_div);
 }
 
 Zotero_PreventDuplicates_Dialog.acceptSelection = function() {
